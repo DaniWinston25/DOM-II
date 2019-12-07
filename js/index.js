0ec0bd3d540900519c0d2be05131c702f31773fc
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-12-06 21:16:14 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// Your code goes here
+//Ten unique events//
+
+//mouseover//
+const navLinks = document.querySelector('nav a')
+console.log(navLinks)
+navLinks.addEventListener("mouseover", (event)=>{
+    console.log(event)
+navLinks.style.color = "rgb(252,188,78)"
+
+setTimeout(()=>{
+    navLinks.style.color ="black"
+},700)
+
+},false)
+
+//click//
+
+
+
+
+
+
+
+
+
+
+
+//mouseenter,mouseleave//
+const textContent = document.querySelector(".text-content p")
+
+imgTop.addEventListener('mouseenter',(event)=>{
+    textContent.style.transform = "scale(1.2)";
+       textContent.style.transition = "all 0.2s";
+     });
+
+     textContent.addEventListener('mouseleave', (event) => {
+        
+        textContent.style.transform = "scale(1.0)";
+          textContent.style.transition = "all 0.2s";
+         });
+
+
